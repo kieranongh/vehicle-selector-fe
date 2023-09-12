c7217f674678219f590df8820a6ad03f8bdc4b81
Kieran <>
2023-09-13 09:55:48 +1000

    Handle model/badge selections

--- src/components/SelectComponent.tsx
+++ src/components/SelectComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-interface SelectOption {
+export interface ISelectOption {
   label: string,
   value: any
 }
@@ -8,16 +8,26 @@ interface SelectComponentProps {
   label: string,
   name: string,
   value: any,
-  options: SelectOption[],
+  options: SelectOption[] | null,
   handleChange: React.ChangeEventHandler<HTMLSelectElement>
 }
 
 export const SelectComponent = ({ label, name, value, options, handleChange }: SelectComponentProps) => {
+  const placeholder = (!options && label) || ''
+
   return (
     <fieldset>
       <legend>{label}</legend>
-      <select className="w-48" name={name} id={name} value={value} onChange={handleChange}>
-        {options.map(opt => {
+      <select
+        className="w-48"
+        name={name}
+        id={name}
+        value={value}
+        onChange={handleChange}
+        disabled={!options}
+        placeholder={placeholder}
+      >
+        {options && options.map(opt => {
           return (
             <option key={opt.value} value={opt.value}>{opt.label}</option>
           )
--- src/components/VehicleSelectorForm.tsx
+++ src/components/VehicleSelectorForm.tsx
@@ -3,19 +3,72 @@ import React from 'react'
 
 import { VEHICLES } from '@/models/vehicle'
 import { capitalise } from '@/util/util'
-import { SelectComponent } from '@/components/SelectComponent'
+import { SelectComponent, ISelectOption } from '@/components/SelectComponent'
 
+const makePlaceHolder = { label: "Select make", value: '' }
+const modelPlaceHolder = { label: "Select model", value: '' }
+const badgePlaceHolder = { label: "Select badge", value: '' }
 
-const makeOptions = Object.keys(VEHICLES).map(make => ({
-  label: capitalise(make),
-  value: make,
-}))
+const makeOptions: ISelectOption[] = [
+  makePlaceHolder,
+  ...Object.keys(VEHICLES).map(make => ({
+    label: capitalise(make),
+    value: make,
+  }))
+]
+
 
 export const VehicleSelectorForm = () => {
   const [make, setMake] = React.useState<string>('')
+  const [model, setModel] = React.useState<string>('')
+  const [badge, setBadge] = React.useState<string>('')
+  
+  const [modelOptions, setModelOptions] = React.useState<ISelectOption[]>([modelPlaceHolder])
+  const [badgeOptions, setBadgeOptions] = React.useState<ISelectOption[]>([badgePlaceHolder])
+
+  React.useEffect(() => {
+    const newModelOptions: ISelectOption[] = []
+
+    if (make) {
+      const models = Object.keys(VEHICLES[make as keyof typeof VEHICLES]).map(model => ({
+        value: model,
+        label: model,
+      }))
+      newModelOptions.push(...models)
+    }
+    newModelOptions.unshift(modelPlaceHolder)
+    setModelOptions(newModelOptions)
+  }, [make])
+
+  React.useEffect(() => {
+    const newBadgeOptions: ISelectOption[] = []
+
+    if (make && model) {
+      const listOfBadges = VEHICLES[make as keyof typeof VEHICLES]
+
+      // Needs stronger typing - not worth the time for this exercise
+      //@ts-expect-error
+      const badges = listOfBadges[model as keyof typeof listOfBadges].map((badge: string) => ({
+        value: badge,
+        label: badge,
+      }))
+      newBadgeOptions.push(...badges)
+    }
+    newBadgeOptions.unshift(badgePlaceHolder)
+    setBadgeOptions(newBadgeOptions)
+  }, [make, model])
 
   const handleMakeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setMake(e.target.value)
+    setModel('')
+    setBadge('')
+  }
+  const handleModelChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setModel(e.target.value)
+    setBadge('')
+  }
+  const handleBadgeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setBadge(e.target.value)
   }
 
   return (
@@ -29,9 +82,26 @@ export const VehicleSelectorForm = () => {
           value={make}
           handleChange={handleMakeChange}
         />
+        <SelectComponent
+          label="Model"
+          name="model"
+          options={modelOptions}
+          value={model}
+          handleChange={handleModelChange}
+        />
+        <SelectComponent
+          label="Badge"
+          name="badge"
+          options={badgeOptions}
+          value={badge}
+          handleChange={handleBadgeChange}
+        />
       </div>
       <div>
-        Make: {make}
+        <h4>Selection</h4>
+        <p>Make: {make}</p>
+        <p>Model: {model}</p>
+        <p>Badge: {badge}</p>
       </div>
     </div>
   )
